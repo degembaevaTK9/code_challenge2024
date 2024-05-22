b530b239185a10612cabaf68e495dac95836333f
Taazim Kandybekovna <tdkoroleva98@gmail.com>
2024-05-22 15:08:54 +0600

    3. Easy SQL: Square Root and Log

--- Codewars/task_8kyu.sql
+++ Codewars/task_8kyu.sql
@@ -15,3 +15,9 @@ SELECT MOD(number1, number2) as mod from decimals;
  SELECT (number1 % number2) as mod from decimals;
 
  
+/* Given the following table 'decimals'. Return a table with two columns (root, log) where the values in root are the square root of those provided in number1 and the values in log are changed to a base 10 logarithm from those in number2.*/
+
+/*3-task*/
+
+SELECT SQRT(number1) as root, LOG10(2) as log FROM decimals;
+
