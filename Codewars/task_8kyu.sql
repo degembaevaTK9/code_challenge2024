82b8ffd9dbc6b5054a17568cd3be4a3803aa5d5f
Taazim Kandybekovna <tdkoroleva98@gmail.com>
2024-05-22 11:06:18 +0600

    1.SQL Basics: Simple WHERE and ORDER BY

---
+++ Codewars/task_8kyu.sql
@@ -0,0 +1,9 @@
+/* For this challenge you need to create a simple SELECT statement that will return all columns from the people table WHERE their age is over 50
+You should return all people fields where their age is over 50 and order by the age descending*/
+
+/*1-task*/
+SELECT id, name, age FROM people
+WHERE age > 50 ORDER BY age desc;
+
+
+
