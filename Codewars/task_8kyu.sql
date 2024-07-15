0e8287b585aee3567af4c518711fe2397229a576
Taazim Kandybekovna <tdkoroleva98@gmail.com>
2024-07-15 14:54:38 +0600

    add folder training sql and 1.3 task solving 1.3

--- Codewars/task_8kyu.sql
+++ Codewars/task_8kyu.sql
@@ -63,3 +63,8 @@ order by employees desc;
 SELECT name, age, semester, mentor, tuition_received FROM students
 where tuition_received = false;
 
+
+/*You are working for a local school, and you are responsible for collecting tuition from students. You have a list of all students, some of them have already paid tution and some haven't. Write a select statement to get a list of all students who haven't paid their tuition yet. The list should include all the data available about these students.*/
+/*7-task*/
+SELECT name, age, semester, mentor, tuition_received FROM students
+where tuition_received = false;
